976251d0677e728b90f0ea76b8f8d199faf8dfd7
Eric Defore <eric.defore@gmail.com>
2017-09-28 13:24:48 -0400

    Remove separate Practice Areas Menu from Footer. Leaving only full Navigation. Referencing #20

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="row">
 
-		<section class="footer-left columns small-12 medium-6">
+		<section class="footer-left columns small-12 medium-9">
 			<div class="row">
 
 				<div class="logo columns small-12">
@@ -36,29 +36,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 			</div>
 		</section>
 
-		<section class="footer-right columns small-12 medium-6">
+		<section class="footer-right columns small-12 medium-3">
 			<div class="row">
 
-				<div class="columns small-12 medium-6">
+				<div class="columns small-12">
 					<?php
 
 					$menu_obj = inosencio_get_menu_by_location( 'footer-a' );
 					wp_nav_menu(
 						array(
 							'theme_location' => 'footer-a',
-							'container'      => false,
-							'items_wrap'     => '<h3>' . esc_html( $menu_obj->name ) . '</h3><ul id=\"%1$s\" class=\"%2$s\">%3$s</ul>',
-						)
-					);
-					?>
-				</div>
-
-				<div class="columns small-12 medium-6">
-					<?php
-					$menu_obj = inosencio_get_menu_by_location( 'footer-b' );
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer-b',
 							'container'      => false,
 							'items_wrap'     => '<h3>' . esc_html( $menu_obj->name ) . '</h3><ul id=\"%1$s\" class=\"%2$s\">%3$s</ul>',
 						)
